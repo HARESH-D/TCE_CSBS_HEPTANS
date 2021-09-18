3e8158a9a5b7301e768e6d812e1fc959f1ba96d0
HARESH-D <73981964+HARESH-D@users.noreply.github.com>
2021-09-18 22:39:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-import keras
+import glob
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout
-from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from keras.optimizers import Adam
-
-
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
-
 import cv2
 import os
-
+import json
 import numpy as np
 
 labels = ['hi', 'background']
 img_size = 64
+
+
 def get_data(data_dir):
     data = []
     for label in labels:
-
-        #path. join() method in Python join one or more path components intelligently
+        
+        #path. join() method in Python join one or more path components intelligently       
         path = os.path.join(data_dir, label)
 
         #index() is an inbuilt function in Python, which searches for a given element
@@ -35,76 +34,85 @@ def get_data(data_dir):
                 print(e)
     return np.array(data)
 
-train = get_data(r"E:\dataset\training")
-val = get_data(r"E:\dataset\test")
 
-x_train = []
-y_train = []
-x_val = []
-y_val = []
+train = get_data(r"E:\dataset\training")
+
+X = []
+y = []
+
 
 for feature, label in train:
-  x_train.append(feature)
-  y_train.append(label)
+  X.append(feature)
+  y.append(label)
 
-for feature, label in val:
-  x_val.append(feature)
-  y_val.append(label)
+test_data = list()
+for img_path in glob.glob("E:\dataset\TTest\*.jpg"):
+    try:
+        img_arr = cv2.imread(img_path)[..., ::-1]  # convert BGR to RGB format
+        resized_arr = cv2.resize(img_arr, (img_size, img_size))  # Reshaping images to preferred size
+        test_data.append(resized_arr)
+    except Exception as e:
+        print(e)
+
 
 # Normalize the data
-x_train = np.array(x_train) / 255
-x_val = np.array(x_val) / 255
+X = np.array(X) / 255
+test_data = np.array(test_data) / 255
 
-x_train.reshape(-1, img_size, img_size, 1)
-y_train = np.array(y_train)
+X.reshape(-1, img_size, img_size, 1)
+y = np.array(y)
 
-x_val.reshape(-1, img_size, img_size, 1)
-y_val = np.array(y_val)
+test_data.reshape(-1, img_size, img_size, 1)
 
-model = Sequential()
-model.add(Conv2D(input_shape=(64,64,3),filters=64,kernel_size=(3,3),padding="same", activation="tanh"))
-model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="tanh"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="tanh"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+(X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
-model.add(Flatten())
-model.add(Dense(128,activation="relu"))
-model.add(Dense(2, activation="softmax"))
+def create_model():
+    model = Sequential()
+    model.add(Conv2D(input_shape=(64, 64, 3), filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation="relu"))
+    model.add(Dense(2, activation="softmax"))
+    model.summary()
+    opt = Adam(lr=0.0001)
+    model.compile(optimizer=opt, loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                  metrics=['accuracy'])
+    return model
 
-model.summary()
 
-opt = Adam(lr=0.000001)
-model.compile(optimizer = opt , loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) , metrics = ['accuracy'])
+opt = Adam(lr=0.0001)
+model = create_model()
 
-history = model.fit(x_train,y_train,epochs = 100 )
 
-predictions = model.predict_classes(x_val)
-print(predictions)
-print(y_val)
+history = model.fit(X, y, epochs=25, validation_data=(X_test, y_test))
+
 
 # serialize model to JSON
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
+
 # serialize weights to HDF5
 model.save_weights("model.h5")
 print("Saved model to disk")
@@ -114,12 +122,39 @@ from keras.models import model_from_json
 json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
+
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
 loaded_model.load_weights("model.h5")
 print("Loaded model from disk")
 
 # evaluate loaded model on test data
-loaded_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) , optimizer=opt, metrics=['accuracy'])
-score = loaded_model.evaluate(x_train, y_train, verbose=0)
-print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
+loaded_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(), optimizer=opt, metrics=['accuracy'])
+predictions = list(loaded_model.predict_classes(test_data))
+print(predictions)
+# print(y_test)
+
+
+# from utils.io import write_json
+
+def write_json(filename, result):
+    with open(filename, 'w') as outfile:
+        json.dump(result, outfile)
+
+
+def read_json(filename):
+    with open(filename, 'r') as outfile:
+        data =  json.load(outfile)
+    return data
+
+def generate_sample_file(filename, x):
+    res = {}
+    for i in range(1,98):
+        test_set = str(i) + '.jpg'
+        res[test_set] = x[i]
+    print("file generate")
+    write_json(filename, res)
+
+
+if __name__ == '__main__':
+    generate_sample_file('./sample_result1.json', predictions)
